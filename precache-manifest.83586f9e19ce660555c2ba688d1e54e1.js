c4d62d972359a71d649818da807a32c941c9e29e
khushi1706 <khushi_shareef@outlook.com>
2020-03-15 13:40:33 +0530

    Updates

---
+++ precache-manifest.83586f9e19ce660555c2ba688d1e54e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd8ecdf9ed0d37ac5aefc8cff642b9e",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "20e0a9acee138be584ae",
+    "url": "/MonstersRolodex/static/css/main.0e1bf287.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/MonstersRolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/MonstersRolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e0a9acee138be584ae",
+    "url": "/MonstersRolodex/static/js/main.0ebbb3da.chunk.js"
+  },
+  {
+    "revision": "c0bea46f4d431cf3e737",
+    "url": "/MonstersRolodex/static/js/runtime-main.d0ae1f9c.js"
+  }
+]);
